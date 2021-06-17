615742a839215d3540e48d8024d8184c741029b8
AlecuMihai <mihai-marian.alecu@my.fmi.unibuc.ro>
2021-06-17 23:02:34 +0300

    5th[...]

--- R/CRV.computeP.R
+++ R/CRV.computeP.R
@@ -3,19 +3,18 @@
 #'
 #' @title  CRV.computeP function & utils
 
-NULL
-
 #' @name %<%
 #' @rdname CRV.computeP
 #' @param  X = X
 #' @param  x = x
 #' @return \code{(X %<% 1)} returns c(X@lo, 1)
 #' @export
-"%<%" <- function(X, x) {
+"%<%" <- function(X, x) {}
+setMethod("%<%", "ContinuousRV", function(X, x){
   if(class(X) == "ContinuousRV") {
     return(c(X@lo, min(x, X@hi)))
   }
-}
+})
 
 #' @name %>%
 #' @rdname CRV.computeP
@@ -66,6 +65,10 @@ setMethod("CRV.computeP", "ContinuousRV", function(obj, x) {
 
   lower.bound <- x[1]
   upper.bound <- x[2]
+  if(lower.bound < X@lo | upper.bound > X@hi) {
+    stop("(_lower.bound, _upper.bound) trebuie sa fie inclus in (X@lo, X@hi)")
+    return()
+  }
   result <- integrate(obj@fun, lower = lower.bound, upper = upper.bound)$value
 
   return(result)
